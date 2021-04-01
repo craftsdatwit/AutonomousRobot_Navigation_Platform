ed6dfe6ec1a0acd8a527625b26377c16a8c8ef1b
David Crafts <craftsd@wit.edu>
2021-04-01 09:35:51 -0400

    Changed order of if statement

--- src/scripts/avoid_navigate.py
+++ src/scripts/avoid_navigate.py
@@ -7,24 +7,17 @@ from sensor_msgs.msg import LaserScan
 
 def goaround(msg):
 
-    stopDistance = 2.0
+    stopDistance = 1.5
 
-    scanDistance = 3.5
+    scanDistance = 3.0
     
-    """
-    bottomrightfree = None
-    toprightfree = None
-    leftFree = None
-    """
 
-    
     def turnRight():
         #Turn Right
         move.linear.x = 0.0
         move.angular.z = -0.2
         pub.publish(move)
 
-    
     def turnLeft():
         #Turn left
         move.linear.x = 0.0
@@ -51,36 +44,26 @@ def goaround(msg):
         right = distances[0:11]
         return min(left+right)
 
-    #Check if the right side is free only if the section is wide enough for the robot
+    #Check if top right is clear
     def checkTopRight():
-        #print("Checking Top Right")
-
         topright = msg.ranges[270:347] #Get the right side range
 
         maxr = max(topright) #Gets the max value of the right side range
-
         indexr = topright.index(maxr) #Gets the index of the maxr value
-
         success =0 #Success variable
-
         toprightfree = False
 
         #If maxr is greater than the stop distance
         if maxr > scanDistance:
-            success +=1 #add 1 to success variable
-
+            success +=1 
             for i in topright[indexr:]: #for i in the right index starting at the max index 
-
-                if i > stopDistance: #if i is greater than the stop distance
-                    #print(i)
+                if i > scanDistance: #if i is greater than the stop distance
                     success +=1 #add one to I
                 else: #if in the scan there's an object 
                     break
-
                 if success == 20: #if an opening has been found
                     break
             if success == 20: #if an opening is found
-                print("Success, opening found: ", success)
                 success = 0
                 toprightfree = True
             else: #if no opening is found
@@ -89,63 +72,45 @@ def goaround(msg):
                 toprightfree = False
         return toprightfree
 
-    #Check if the right side is free only if the section is wide enough for the robot
+    #Check if bottom right is clear
     def checkBottomRight():
-        #print("Checking Bottom Right")
-
-        #Get the right side range
         bottomright = msg.ranges[181:269]
 
-        #Gets the max value of the right side range
         maxr = max(bottomright) 
-
-        #Gets the index of the maxr value
-        indexr = bottomright.index(maxr) #Gets the index of the maxr value
-        
-        #Success variable
+        indexr = bottomright.index(maxr) 
         success =0 
-
         bottomrightfree = False
 
-        #If maxr is greater than the stop distance
         if maxr > scanDistance:
-            print(maxr) 
-            success +=1 #add 1 to success variable
-
-            for i in bottomright[indexr:]: #for i in the right index starting at the max index 
-
-                if i > stopDistance: #if i is greater than the stop distance
-                    #print(i)
-                    success +=1 #add one to I
-                else: #if in the scan there's an object 
+            success +=1 
+            for i in bottomright[indexr:]: 
+                if i > scanDistance:
+                    success +=1 
+                else: 
                     break
-
-                if success == 20: #if an opening has been found
+                if success == 20: 
                     break
-            if success == 20: #if an opening is found
+            if success == 20: 
                 success = 0
                 bottomrightfree = True
-            else: #if no opening is found
+            else: 
                 success = 0
                 bottomrightfree = False
             return bottomrightfree
 
-
+    #Check if top left is clear
     def checkTopLeft():
-        #print("Checking top Left")
-    
         left = msg.ranges[12:90] 
+
         maxl = max(left)
         indexl = left.index(maxl)
         success = 0
-
         leftFree = False
 
         if maxl > scanDistance:
             success +=1
             for i in left[indexl:]:
-                if i > stopDistance:
-                    #print(i)
+                if i > scanDistance:
                     success +=1
                 else:
                     break
@@ -159,23 +124,19 @@ def goaround(msg):
                 leftFree = False
         return leftFree
 
-
+    #Check if bottom left is clear
     def checkBottomLeft():
-        #print("Checking bottom Left")
-
         bottomleft = msg.ranges[91:179] 
+
         maxl = max(bottomleft)
         indexl = bottomleft.index(maxl)
         success = 0
-
         bottomleftFree = False
 
         if maxl > scanDistance:
-            print(maxl)
             success +=1
             for i in bottomleft[indexl:]:
-                if i > stopDistance:
-                    #print(i)
+                if i > scanDistance:
                     success +=1
                 else:
                     break
@@ -191,22 +152,39 @@ def goaround(msg):
 
 
     global waited
-
-
-    #Define stopping distance
-    stopDistance = 1.2
+    global topleft 
+    global topright
+    global bottomright 
+    global bottomleft 
 
     #Get closest distance from center ranges
     center =  minCenterDistance()
 
-    #If the center is blocked
-    if center < stopDistance and waited:
-        #print("If center is blocked and has waited")
+
+    if center < stopDistance and not waited:
+        print("Center is blocked and has not waited")
+        #Stop robot
+        stopMoving()
+
+        #Set flag
+        print("Waiting now true")
+        waited = True
+
+        #SCAN
 
         topleft = checkTopLeft()
         topright = checkTopRight()
         bottomright = checkBottomRight()
         bottomleft = checkBottomLeft()
+
+        #Set time
+        time.sleep(0)
+
+
+
+        #If the center is blocked
+    if center < stopDistance and waited:
+        #print("If center is blocked and has waited")
 
         if topright == True: #if right is free
             print("Turning Right")
@@ -226,20 +204,6 @@ def goaround(msg):
             turnLeft()
             print("Top Right / Left and Bottom Right are blocked")
         
-
-        
-            
-    if center < stopDistance and not waited:
-        print("Center is blocked and has not waited")
-        #Stop robot
-        stopMoving()
-
-        #Set flag
-        print("Waiting now true")
-        waited = True
-
-        #Set time
-        time.sleep(1)
 
     elif center >= stopDistance:
         waited = False
